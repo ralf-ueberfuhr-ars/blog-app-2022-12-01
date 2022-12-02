662b72e5e38216d64981b79969a71be7b0638ee0
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-02 09:46:19 +0100

    Add controller method to find single post

--- src/main/java/de/schulung/samples/blog/BlogPostController.java
+++ src/main/java/de/schulung/samples/blog/BlogPostController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
@@ -19,6 +20,12 @@ public class BlogPostController {
     @ResponseBody
     public Collection<BlogPost> getBlogPosts() {
         return service.findPosts();
+    }
+
+    @GetMapping("/find")
+    @ResponseBody
+    public BlogPost getBlogPost(@RequestParam("id") long id) {
+        return service.findPostById(id).orElse(null);
     }
 
 }
