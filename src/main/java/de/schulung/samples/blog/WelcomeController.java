a1ff6ba76577958042a79ef1cc6e49bdb3dc4501
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-05 10:03:26 +0100

    Make welcome page dynamic (JSP)

---
+++ src/main/java/de/schulung/samples/blog/WelcomeController.java
@@ -0,0 +1,18 @@
+package de.schulung.samples.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * We need the separate class instead of registering via WebMvcConfigurer,
+ * because the ControllerAdvice does only work for this solution.
+ */
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/index.html")
+    public String welcome() {
+        return "index";
+    }
+
+}
