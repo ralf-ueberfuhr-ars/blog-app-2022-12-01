a4a4345faba6f89c73a63a393618a75f1fce336d
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-13 16:37:10 +0100

    Use @TestConfiguration

--- src/test/java/de/schulung/samples/blog/boundary/config/TestSecurityConfiguration.java
+++ src/test/java/de/schulung/samples/blog/boundary/config/TestSecurityConfiguration.java
@@ -1,11 +1,11 @@
 package de.schulung.samples.blog.boundary.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@TestConfiguration
 public class TestSecurityConfiguration {
 
     @Bean
