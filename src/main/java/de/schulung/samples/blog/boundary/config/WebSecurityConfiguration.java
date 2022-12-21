2e43d957307bb23de4e4d902f407530f15afe0dd
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-21 01:05:34 +0100

    Disable CSRF for REST API

--- src/main/java/de/schulung/samples/blog/boundary/config/WebSecurityConfiguration.java
+++ src/main/java/de/schulung/samples/blog/boundary/config/WebSecurityConfiguration.java
@@ -3,14 +3,19 @@ package de.schulung.samples.blog.boundary.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain disableCsrfForRestApiChain(HttpSecurity http) throws Exception {
+        http.formLogin();
+        http.httpBasic();
+        http.authorizeHttpRequests().anyRequest().permitAll();
         http
           .csrf()
           .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/api/**"))
